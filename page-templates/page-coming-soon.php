b5360b285cd079bda757ce2c51f82385788ea658
Daniel-Digid <daniel@dig.id>
2022-07-13 16:21:28 +0100

    Dev

---
+++ page-templates/page-coming-soon.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Coming Soon Page Template
+ */
+?>
+<?php get_header(); ?>
+<div id="coming-soon__section" class="container-fluid coming-soon__container">
+    <div class="row w-100 coming-soon__half coming-soon__tophalf">
+        <div class="col-sm-6">
+            <img class="coming-soon__logo" src="<?php echo wp_upload_dir()['url'] . '/hotel-inside-logo-vertical.svg' ?>" alt="" title="">
+            <h1 class="coming-soon__toptext">Hier entsteht Ihre neue Plattform und Community für die nationale und internationale Hotellerie!</h1>
+        </div>
+    </div>
+    <div class="row w-100 coming-soon__half coming-soon__bothalf">
+        <div class="col-sm-6 mmenu_screen--direktaction">
+            <p class="coming-soon__bottext">Werden Sie Hotel- und Hospitality-Insider!<br>Abonnieren Sie unseren Newsletter und erhalten Sie die relevantesten Insights und die aktuellsten Kommentare und Videobeiträge von Hans R. Amrein.</p>
+        </div>
+    </div>
+</div>
+<?php 
