92157dc9466055f00a7eba3d68368458053b006b
pedro-digid <pedro@dig.id>
2022-07-14 15:03:28 +0100

    up

--- template-parts/modules/section-newsletter-cta.php
+++ template-parts/modules/section-newsletter-cta.php
@@ -1,8 +1,22 @@
+<?php
+$newsletter_title       = get_theme_mod( 'newsletter_cta_title' );
+$newsletter_description = get_theme_mod( 'newsletter_cta_description' );
+$newsletter_shortcode   = get_theme_mod( 'newsletter_cta_sc' );
+?>
 <section class="section section-newsletter-cta">
 	<div class="container">
 		<div class="row">
-			<div class="col-12 text-center">
-				<h2 class="section-title">Newsletter</h2>
+			<div class="col-6 text-center">
+				<h2 class="section-title"><?php echo esc_html( $newsletter_title ); ?></h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-6">
+				<?php
+				if ( $newsletter_shortcode ) :
+
+				endif;
+				?>
 			</div>
 		</div>
 	</div>
