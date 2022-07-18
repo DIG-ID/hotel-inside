f712a84d3ecad5cb2ca300fd890358e47ddbec1a
Daniel-Digid <daniel@dig.id>
2022-07-18 16:50:52 +0100

    Dev

--- page-templates/page-martkplatz.php
+++ page-templates/page-martkplatz.php
@@ -8,5 +8,6 @@ do_action( 'before_main_content' );
 	get_template_part( 'template-parts/modules/section', 'breadcrumbs' );
 	get_template_part( 'template-parts/marktplatz/section', 'intro' );
 	get_template_part( 'template-parts/marktplatz/section', 'marktplatz' );
+	get_template_part( 'template-parts/marktplatz/section', 'outro' );
 do_action( 'after_main_content' );
 get_footer();
---
+++ template-parts/marktplatz/section-outro.php
@@ -0,0 +1,10 @@
+<div id="section-markt-outro" class="section section-markt-outro">
+    <div class="container">
+        <div class="row markt-outro__row">
+            <div class="col-6">
+                <p class="markt-outro__text"><?php _e( 'Sie haben ihr Unternehmen nicht gefunden? Dann kontaktieren Sie uns!', 'hotel-inside' ); ?></p>
+                <a href="#" class="btn btn__blue"><?php _e( 'Kontaktieren Sie uns', 'hotel-inside' ); ?></a>
+            </div>
+        </div>
+    </div>
+</div>
