5de662eecbb10992356702a382d9cc0901a57aea
pedro-digid <pedro@dig.id>
2022-07-20 16:10:32 +0100

    up

--- template-parts/modules/section-newsletter-cta.php
+++ template-parts/modules/section-newsletter-cta.php
@@ -6,7 +6,7 @@ $newsletter_sc = get_theme_mod( 'newsletter_sc' );
 		<div class="row justify-content-center align-items-center">
 			<div class="col-6 text-center">
 				<h2 class="section-title"><?php esc_html_e( 'Newsletter', 'hotel-inside' ); ?></h2>
-				<p class="section-description"><?php esc_html_e( 'Newsletter', 'hotel-inside' ); ?></p>
+				<p class="section-description"><?php esc_html_e( 'Werden Sie auch ein Insider!', 'hotel-inside' ); ?></p>
 			</div>
 		</div>
 		<div class="row justify-content-center align-items-center">
