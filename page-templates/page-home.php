9fcc3e214e14f0f1c57629163eea98c898637cda
pedro-digid <pedro@dig.id>
2022-07-21 16:07:46 +0100

    up

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -9,7 +9,7 @@ do_action( 'before_main_content' );
 	get_template_part( 'template-parts/modules/section', 'slider-w-sidebar' );
 	get_template_part( 'template-parts/modules/section', 'three-blocks' );
 	get_template_part( 'template-parts/modules/section', 'marktplatz' );
-	get_template_part( 'template-parts/modules/section', 'club-events' );
+	//get_template_part( 'template-parts/modules/section', 'club-events' );
 	get_template_part( 'template-parts/modules/section', 'latest-videos' );
 	get_template_part( 'template-parts/modules/section', 'four-blocks' );
 	get_template_part( 'template-parts/modules/section', 'ads-footer' );
