c1bc438a652da412dc48f89bf1e85d22b6399c83
Daniel-Digid <daniel@dig.id>
2022-07-21 16:49:06 +0100

    Dev

--- template-parts/marktplatz/content-standart.php
+++ template-parts/marktplatz/content-standart.php
@@ -15,8 +15,8 @@
 					<div class="col-lg-6">
 						<p class="socials-text"><?php esc_html_e( 'Folgen Sie uns', 'hotel-inside' ); ?></p>
 						<div class="socials-wrapper">
-							<?php if(the_field('facebook')): ?>
-							<a href="<?php the_field('facebook'); ?>" target="_blank" class="social-link social-link__facebook">
+							<?php if(the_field('company_socials_facebook')): ?>
+							<a href="<?php the_field('company_socials_facebook'); ?>" target="_blank" class="social-link social-link__facebook">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
 							</a>
 							<?php endif; ?>
