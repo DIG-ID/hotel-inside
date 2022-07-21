5e03609034c1c4790f011b0e3f01a86440435d05
Daniel-Digid <daniel@dig.id>
2022-07-21 15:05:47 +0100

    Dev

---
+++ archive-marktplatz.php
@@ -0,0 +1,10 @@
+<?php 
+get_header();
+do_action( 'before_main_content' );
+	get_template_part( 'template-parts/modules/section', 'breadcrumbs' );
+	get_template_part( 'template-parts/marktplatz/section', 'intro' );
+	get_template_part( 'template-parts/marktplatz/section', 'marktplatz-ajax' );
+	get_template_part( 'template-parts/marktplatz/section', 'outro' );
+do_action( 'after_main_content' );
+get_footer();
+?>
--- template-parts/marktplatz/content-standart.php
+++ template-parts/marktplatz/content-standart.php
@@ -3,12 +3,17 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-6">
-				<?php the_title( '<h1 class="title">', '</h1>' ); ?>
-				<?php if (has_post_thumbnail( $post->ID ) ): ?>
+				<?php the_title( '<h1 class="company-content__title">', '</h1>' ); ?>
+				<img src="<?php the_field('company_banner_image'); ?>">
+				<div class="row company-content_row">
+					<div class="col-lg-3">
+					<?php if (has_post_thumbnail( $post->ID ) ): ?>
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-					<img src="<?php echo $image[0]; ?>" title="featured image" alt="featured image">
-				<?php endif; ?>
-
+						<img src="<?php echo $image[0]; ?>" title="featured image" alt="featured image">
+					<?php endif; ?></div>
+					<div class="col-lg-3"><?php the_field('company_contact_info'); ?></div>
+					<div class="col-lg-6"></div>
+				</div>
 				<div class="content">
 					<?php the_content(); ?>
 				</div>
