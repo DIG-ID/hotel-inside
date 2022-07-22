f8cc382c1f5594bfba26d50201c077b16e56b973
Daniel-Digid <daniel@dig.id>
2022-07-22 16:10:26 +0100

    Dev

--- single.php
+++ single.php
@@ -4,7 +4,10 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		$format = get_post_format() ? : 'standart';
+		do_action( 'before_main_content' );
+		get_template_part( 'template-parts/modules/section', 'breadcrumbs' );
 		get_template_part( 'template-parts/post/content', $format );
+		do_action( 'after_main_content' );
 	endwhile;
 endif;
 get_footer();
--- template-parts/modules/section-breadcrumbs.php
+++ template-parts/modules/section-breadcrumbs.php
@@ -5,6 +5,8 @@
                 <div class="breadcrumbs__separator-line"></div>
                 <?php if ( is_singular( 'marktplatz' ) ) { ?>
                     <h2 class="breadcrumbs__title">Marktplatz</h2>
+                <?php } elseif (is_single()) { ?>
+                    <h2 class="breadcrumbs__title"><?php echo get_the_category( $id )[0]->name; ?></h2>
                 <?php } else { ?>
                     <h1 class="breadcrumbs__title"><?php the_title(); ?></h1>
                 <?php } ?>
