aa33cb0f1b62423500558ed15f112d3ba524de47
pedro-digid <pedro@dig.id>
2022-07-27 15:18:51 +0100

    up

--- single-marktplatz.php
+++ single-marktplatz.php
@@ -3,10 +3,9 @@ get_header();
 if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
-		$format = get_post_format() ? : 'standart';
 		do_action( 'before_main_content' );
 		get_template_part( 'template-parts/modules/section', 'breadcrumbs' );
-		get_template_part( 'template-parts/marktplatz/content', $format );
+		get_template_part( 'template-parts/marktplatz/content', 'standart' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
