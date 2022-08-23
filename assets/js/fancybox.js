32cfb47c6c21f9ff8605d52bb981038373c4edaf
Daniel-Digid <daniel@dig.id>
2022-08-23 11:31:40 +0100

    Dev

--- assets/js/fancybox.js
+++ assets/js/fancybox.js
@@ -3,12 +3,7 @@ import { Fancybox } from "@fancyapps/ui";
 $(function() {
 
 	Fancybox.bind(".wp-block-gallery figure img", {
-        groupAll : true, // Group all items
-        on : {
-          ready : (fancybox) => {
-            console.log(`fancybox #${fancybox.id} is ready!`);
-          }
-        }
+        groupAll : true,
       });
 	
 });
