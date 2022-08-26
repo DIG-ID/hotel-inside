ae0fff32ebe21e016c4164bd70b4fc9239e91476
Daniel-Digid <daniel@dig.id>
2022-08-26 12:38:24 +0100

    Dev

--- template-parts/mega-menu/socials.php
+++ template-parts/mega-menu/socials.php
@@ -2,7 +2,7 @@
 <div class="col-lg-4">
 	<h3 class="mega-menu__title"><?php esc_html_e( 'Newsletter', 'hotel-inside' ); ?></h3>
 	<p class="socials-text"><?php esc_html_e( 'Werden auch Sie ein Insider!', 'hotel-inside' ); ?></p>
-	<?php if ( $newsletter_sc ) : ?>
+	<?php if ( ! empty( $newsletter_sc ) ) : ?>
 		<?php echo do_shortcode( $newsletter_sc ); ?>
 	<?php endif; ?>
 	<p class="socials-text"><?php esc_html_e( 'Folgen Sie uns', 'hotel-inside' ); ?></p>
