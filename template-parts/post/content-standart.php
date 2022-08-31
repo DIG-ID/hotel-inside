6db1b2b352ab6109b79d4b5ff444356b0d8d3f82
pedro-digid <pedro@dig.id>
2022-08-31 18:21:40 +0100

    up

--- template-parts/post/content-standart.php
+++ template-parts/post/content-standart.php
@@ -1,28 +1,26 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-	<div id="section-single-post" class="section section-single-post">
-		<div class="custom-container">
-			<div class="row content-row">
-				<div class="col-md-12 col-lg-7 content">
-					<?php
-					if ( has_post_thumbnail() ) :
-						the_post_thumbnail( 'single-post-thumbnail' );
-					endif;
-					?>
-					<header class="header single-post__header">
-						<div class="single-post__date"><i class="icon-clock"></i><?php echo get_the_date( 'd M, Y' ); ?></div>
-						<?php the_title( '<h1 class="title">', '</h1>' ); ?>
-					</header>
-					<div class="single-post__content">
-						<?php the_content(); ?>
-						<?php do_action( 'back_button' ); ?>
-						<div class="latest-posts__linesep"></div>
-					</div>
+	<div class="custom-container">
+		<div class="row">
+			<div class="col-md-12 col-lg-7 content">
+				<?php
+				if ( has_post_thumbnail() ) :
+					the_post_thumbnail( 'single-post-thumbnail' );
+				endif;
+				?>
+				<header class="header single-post__header">
+					<div class="single-post__date"><i class="icon-clock"></i><?php echo get_the_date( 'd M, Y' ); ?></div>
+					<?php the_title( '<h1 class="title">', '</h1>' ); ?>
+				</header>
+				<div class="single-post__content">
+					<?php the_content(); ?>
+					<?php do_action( 'back_button' ); ?>
+					<hr>
 				</div>
-				<aside class="col-md-12 col-lg-5 sidebar">
-					<?php get_template_part( 'template-parts/post/content', 'sidebar' ); ?>
-				</aside>
-			</div><!-- .row -->
-			<?php get_template_part( 'template-parts/post/related-posts' ); ?>
-		</div>
+			</div>
+			<aside class="col-md-12 col-lg-5 sidebar">
+				<?php get_template_part( 'template-parts/post/content', 'sidebar' ); ?>
+			</aside>
+		</div><!-- .row -->
+		<?php get_template_part( 'template-parts/post/related-posts' ); ?>
 	</div>
 </article>
