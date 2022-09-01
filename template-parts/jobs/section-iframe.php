25fe6bb20ac725fffe0523f47f0653aa8cdc6847
pedro-digid <pedro@dig.id>
2022-09-01 10:33:59 +0100

    up

--- template-parts/jobs/section-iframe.php
+++ template-parts/jobs/section-iframe.php
@@ -2,7 +2,7 @@
 	<div class="custom-container">
 		<div class="row">
 			<div class="col-12">
-				<iframe style="width: 100%; height: 400px;" src="https://jobs.hotelcareer.de/index.php?sei_id=321&amp;pid=516" width="100" height="100" scrolling="no" data-dashlane-frameid="926"></iframe>
+				<iframe style="width: 100%; height: 2000px;" src="https://jobs.hotelcareer.de/index.php?sei_id=321&amp;pid=516" width="100" height="100" scrolling="no" data-dashlane-frameid="926"></iframe>
 			</div>
 		</div>
 	</div>
