8d16814f7cc6284a01c91e431a2dca7cf3ac8498
pedro-digid <pedro@dig.id>
2022-09-02 18:05:38 +0100

    up

--- searchform.php
+++ searchform.php
@@ -5,6 +5,6 @@ $hi_aria_label = ! empty( $args['aria_label'] ) ? 'aria-label="' . esc_attr( $ar
 <form role="search" <?php echo $hi_aria_label; ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group has-search">
 		<span class="fa fa-search form-control-feedback"></span>
-		<input type="search"  id="<?php echo esc_attr( $hi_unique_id ); ?>" class="form-control search-field" value="<?php echo get_search_query(); ?>" name="s" placeholder="Search">
+		<input type="search"  id="<?php echo esc_attr( $hi_unique_id ); ?>" class="form-control search-field" value="<?php echo get_search_query(); ?>" name="s" placeholder="Suchen">
 	</div>
 </form>
