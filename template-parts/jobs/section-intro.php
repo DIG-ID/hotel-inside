8c1c970013a9bd4a7d27ed1879653ef4937015cf
Daniel-Digid <daniel@dig.id>
2022-09-05 17:17:17 +0100

    Dev

--- template-parts/jobs/section-intro.php
+++ template-parts/jobs/section-intro.php
@@ -1,7 +1,7 @@
 <section class="section section-intro">
 	<div class="custom-container">
 		<div class="row justify-content-center align-items-center">
-			<div class="col-6 text-center">
+			<div class="col-12 col-sm-10 col-md-6 col-lg-6 col-xl-6 text-center">
 				<p>
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit illo, officiis accusantium atque placeat id tempora consequuntur amet rem minus quia eligendi culpa inventore optio molestiae modi, qui asperiores sequi.
 				</p>
