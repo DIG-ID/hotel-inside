d9ae3d73f5d80abcf860f00a30e668925e13340a
pedro-digid <pedro@dig.id>
2022-09-05 14:41:06 +0100

    up

--- template-parts/search/no-results.php
+++ template-parts/search/no-results.php
@@ -3,12 +3,17 @@
 		<div class="custom-container">
 			<div class="row">
 				<div class="col-12">
+					<h1 class="section-title"><?php esc_html_e( 'Nichts gefunden', 'hotel-inside' ); ?></h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-7 col-lg-7">
 					<div class="page-content default-max-width">
 						<?php if ( is_search() ) : ?>
-							<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'hotel-inside' ); ?></p>
+							<p><?php esc_html_e( 'Entschuldigung, aber nichts entspricht Ihren Suchbegriffen. Bitte versuchen Sie es erneut mit anderen Keywords. Hier geht es zurück zur Startseite', 'hotel-inside' ); ?> <a href="<?php echo get_home_url(); ?>"><?php esc_html_e( 'zur Startseite','hotel-inside' ); ?></a></p>
 							<?php get_search_form(); ?>
 						<?php else : ?>
-							<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'hotel-inside' ); ?></p>
+							<p><?php esc_html_e( 'Anscheinend können wir nicht finden, wonach Sie suchen. Vielleicht hilft die Suche.', 'hotel-inside' ); ?></p>
 							<?php get_search_form(); ?>
 						<?php endif; ?>
 					</div><!-- .page-content -->
