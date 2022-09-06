6f98909f71683cd0c15acac99a242dd71eb4603b
Daniel-Digid <daniel@dig.id>
2022-09-06 15:03:59 +0100

    Dev

--- assets/js/sticky-sidebar-starter.js
+++ assets/js/sticky-sidebar-starter.js
@@ -3,7 +3,7 @@ import stickySidebar from 'sticky-sidebar';
 $(function() {
 if( $('body.single-post').length ){
     var sidebarbyhans = new StickySidebar('.sidebar', {
-        containerSelector: '.section > .custom-container > .content-row',
+        containerSelector: '.section > .custom-container > .row',
         innerWrapperSelector: '.sidebar__inner',
         topSpacing: 200,
         bottomSpacing: 20
@@ -11,7 +11,7 @@ if( $('body.single-post').length ){
 }
 if( $('body.single-kommentar_by_hans').length ){
     var sidebarbyhans = new StickySidebar('.sidebar-by-hans', {
-        containerSelector: '.section > .custom-container > .content-row',
+        containerSelector: '.section > .custom-container > .row',
         innerWrapperSelector: '.sidebar-by-hans__inner',
         topSpacing: 200,
         bottomSpacing: 200
