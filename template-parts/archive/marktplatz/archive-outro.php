5e37f741265375add20739313baacb1a520d039a
Daniel-Digid <daniel@dig.id>
2022-09-06 16:32:25 +0100

    Dev

--- template-parts/archive/marktplatz/archive-outro.php
+++ template-parts/archive/marktplatz/archive-outro.php
@@ -3,7 +3,7 @@
 		<div class="row markt-outro__row">
 			<div class="col-3 p-0 markt-outro__col">
 				<p class="markt-outro__text"><?php esc_html_e( 'Sie haben ihr Unternehmen nicht gefunden? Dann kontaktieren Sie uns!', 'hotel-inside' ); ?></p>
-				<a href="#" class="btn btn__blue"><?php esc_html_e( 'Kontaktieren Sie uns', 'hotel-inside' ); ?></a>
+				<a href="/uber-uns/" class="btn btn__blue"><?php esc_html_e( 'Kontaktieren Sie uns', 'hotel-inside' ); ?></a>
 			</div>
 		</div>
 	</div>
