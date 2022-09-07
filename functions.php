e1c2bf403c81308441d3a9874ee5afc87cbec0e0
pedro-digid <pedro@dig.id>
2022-09-07 12:10:17 +0100

    up

--- functions.php
+++ functions.php
@@ -210,6 +210,8 @@ function hi_preload_webfonts() {
 
 add_action( 'wp_head', 'hi_preload_webfonts' );
 
+add_filter( 'jpeg_quality', function($arg) {return 100;} );
+
 /**
  * Change the default WordPress titles
  */
@@ -247,13 +249,3 @@ require get_template_directory() . '/inc/custom-nav-walker.php';
 
 // Theme custom ajax loader
 require get_template_directory() . '/inc/ajax-loader.php';
-
-
-function console_log($output, $with_script_tags = true) {
-	$js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-	if ($with_script_tags) {
-			$js_code = '<script>' . $js_code . '</script>';
-	}
-	echo $js_code;
-}
