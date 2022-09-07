9c95289c51b3f099bdf80640ada3596629726312
Daniel-Digid <daniel@dig.id>
2022-09-07 16:49:18 +0100

    Dev

--- assets/js/show-more-text.js
+++ assets/js/show-more-text.js
@@ -1,19 +1,32 @@
 jQuery(document).ready(function($) {   
     if( $('body.single').length ){
     var h = $('.author__description')[0].scrollHeight;
-    $('#showmore').click(function(e) {
+    $('#showmore').on('click', function(e) {
         e.stopPropagation();
-        $('.author__description').animate({
-            'height': h
-        });
-        $("#showmore").html($("#showmore").html().replace('Weiterlesen...','---'));
-        $("#showmore").addClass('itsopen');
+        if( $('.itsclosed').length ){
+            $('.author__description').animate({
+                'height': h
+            });
+            $("#showmore").html($("#showmore").html().replace('Weiterlesen...','Einklappen'));
+            $("#showmore").removeClass('itsclosed');
+            $("#showmore").addClass('itsopen');
+        }
+        else if( $('.itsopen').length ){
+            $('.author__description').animate({
+                'height': '70px'
+            });
+            $("#showmore").html($("#showmore").html().replace('Einklappen','Weiterlesen...'));
+            $("#showmore").removeClass('itsopen');
+            $("#showmore").addClass('itsclosed');
+        }
     });
-    $(document).click(function() {
+    $(document).on('click', function() {
         $('.author__description').animate({
             'height': '70px'
         });
-        $("#showmore").html($("#showmore").html().replace('---','Weiterlesen...'));
+        $("#showmore").html($("#showmore").html().replace('Einklappen','Weiterlesen...'));
+        $("#showmore").removeClass('itsopen');
+        $("#showmore").addClass('itsclosed');
     });
 }
 });
