e909c79d630a30ff9c0cb71ee56c24d4139cdaa4
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-02-09 13:34:10 +0000

    up

--- template-parts/components/card-marktplatz.php
+++ template-parts/components/card-marktplatz.php
@@ -1,18 +1,20 @@
 <div class="col-sm-12 col-md-6 col-lg-4">
 	<div class="marktplatz__col">
 		<h2 class="marktplatz__title active"><?php the_title(); ?> <br> <?php the_field( 'second_line_title' ); ?></h2>
-		<div class="marktplatz__position"><?php the_excerpt(); ?><br></div>
+		<div class="marktplatz__position"><?php the_excerpt(); ?></div>
 		<?php
 		$markt_id    = get_the_ID();
 		$markt_terms = get_the_terms( $markt_id, 'categories_marktplatz' );
 		$checkifpaid = get_field( 'single_page_access' );
 		if ( $markt_terms ) :
+			echo '<p class="marktplatz__cat--wrapper">';
 			foreach ( array_slice($markt_terms, 0, 3) as $markt_term ) :
-				echo '<p class="marktplatz__cat">' . $markt_term->name . '</p>';
+				echo '<span class="marktplatz__cat">' . $markt_term->name . '</span>';
 			endforeach;
+			echo '</p>';
 		endif;
 		if ( $checkifpaid ) :
-			echo '<a href="' . get_permalink() . '" class="marktplatz__read-more m-0">Mehr erfahren<i class="icon-arrow"></i></a>';
+			echo '<div class="marktplatz__read-more--wrapper"><a href="' . get_permalink() . '" class="marktplatz__read-more m-0">Mehr erfahren<i class="icon-arrow"></i></a></div>';
 		endif;
 		?>
 	</div>
