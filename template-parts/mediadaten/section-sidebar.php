d5497ac61e8130b9ef71358f7ada7021e541adda
Daniel-Digid <daniel@dig.id>
2023-06-12 16:49:51 +0100

    dev

--- template-parts/mediadaten/section-sidebar.php
+++ template-parts/mediadaten/section-sidebar.php
@@ -4,6 +4,13 @@ if ( get_field( 'sidebar_box_box_title' ) ) :
 	<div class="row">
 		<div class="col-12">
 			<div class="sidebar__box">
+				<?php 
+				$image = get_field('image');
+				$size = 'full';
+				if( $image ) {
+					echo wp_get_attachment_image( $image, $size );
+				}	
+				?>
 				<h3><?php the_field( 'sidebar_box_box_title' ); ?></h3>
 				<p><?php the_field( 'sidebar_box_description' ); ?></p>
 			</div>
