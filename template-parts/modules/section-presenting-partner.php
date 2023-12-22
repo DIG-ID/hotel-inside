f0f408ba87968dd1863b4658ce9c8a6f5a4f52be
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-12-22 17:00:57 +0000

    up

--- template-parts/modules/section-presenting-partner.php
+++ template-parts/modules/section-presenting-partner.php
@@ -9,14 +9,14 @@ if ( have_rows( 'partners', 'presenting_partner' ) ) :
 				</div>
 			</div>
 			<div class="row justify-content-center align-items-center">
-				<div class="col-12 swiper swiper-presenting-partner">
-					<div class="swiper-wrapper row">
+				<div class="swiper swiper-presenting-partner">
+					<div class="swiper-wrapper">
 						<?php
 					while ( have_rows( 'partners', 'presenting_partner' ) ) :
 						the_row();
 						$partner_logo = get_sub_field( 'logo' );
 						$partner_url  = get_sub_field( 'link_url' );
-						echo '<div class="swiper-slide col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 text-center presenting-partner-card">';
+						echo '<div class="swiper-slide text-center presenting-partner-card">';
 						if ( $partner_logo && $partner_url ) :
 							echo '<a href="' . esc_url( $partner_url ) . '" target="_blank" class="presenting-partner-link">' . wp_get_attachment_image( $partner_logo, 'markplatz-avatar', false, array( 'class' => 'presenting-partner-logo' ) ) . '</a>';
 						else :
