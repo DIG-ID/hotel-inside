acbc84961fa1b195d59be5c081ae2f8ef15f57fd
Daniel-Digid <daniel@dig.id>
2025-07-11 14:04:45 +0100

    up

--- template-parts/newsletter/content.php
+++ template-parts/newsletter/content.php
@@ -12,23 +12,21 @@
 <section class="section section-newsletter">
     <div class="custom-container">
         <div class="row">
-            <!-- Content Column (8/12 width) -->
             <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
-                <p class="newsletter-info__title">Werden Sie ein Insider!</p>
-                <p class="newsletter-info__text">
-                    Abonnieren Sie unseren Newsletter und erhalten Sie die neuesten Insights sowie wöchentliche Schwerpunkt-Themen von Hans R. Amrein.
-                </p>
-
+                <p class="newsletter-info__title"><?php echo get_field( 'newsletter_title' ); ?></p>
+                <p class="newsletter-info__text"><?php echo get_field( 'newsletter_text' ); ?></p>
                 <ul class="newsletter-info__list">
-                    <li>Immer die neuesten Insights</li>
-                    <li>Exklusive Hintergrundberichte</li>
-                    <li>Studien zu aktuellen Themen</li>
-                    <li>Zugang zu zahlreichen Experten</li>
-                    <li>Werden Sie Teil einer exklusiven Community</li>
+                    <?php
+                    if( have_rows('newsletter_list') ):
+                        while( have_rows('newsletter_list') ) : the_row();
+                        ?>
+                            <li><?php echo get_sub_field('item'); ?></li>
+                        <?php 
+                        endwhile;
+                    endif;
+                    ?>
                 </ul>
             </div>
-
-            <!-- Empty Column (4/12 width) -->
             <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4" style="padding-top: 5px;">
                 <?php $kontakt_form = get_field( 'newsletter_form_shortcode' );?>
 				<?php echo do_shortcode( $kontakt_form ); ?>
